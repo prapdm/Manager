33bac97fc390e4ad17e16e40081422515eb6fc61
Paweł <prapdm@users.noreply.github.com>
2021-10-17 00:28:27 +0200

    chore: add Error model

---
+++ UserManagment/Models/Error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Manager.Models
+{
+    public class Error
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
