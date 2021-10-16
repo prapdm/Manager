e94aa50874d460ce6bd9753f1fd82d9c277a7984
Paweł <prapdm@users.noreply.github.com>
2021-10-17 00:27:29 +0200

    chore: add ErrorLoggingMiddleware

---
+++ UserManagment/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Manager.Exeptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace Manager.Middleware
+{
+    public class ErrorLoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorLoggingMiddleware> _logger;
+
+        public ErrorLoggingMiddleware(ILogger<ErrorLoggingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+             
+        }
+    }
+}
