da934f0a8d8fd91e76f15b8f9c949a931b710db4
Paweł <prapdm@users.noreply.github.com>
2021-10-17 00:22:22 +0200

    chore: add ErrorController

---
+++ UserManagment/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Manager.Exeptions
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Exeption()
+        {
+            return View(new Error { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Code(int id)
+        {
+            TempData["id"] = id;
+            return View();
+        }
+
+
+    }
+}
