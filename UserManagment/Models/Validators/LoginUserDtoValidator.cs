e35cfebe2921ab2c23973d82687cb157b4bda6ac
Paweł <prapdm@users.noreply.github.com>
2021-10-17 00:28:03 +0200

    chore: add LoginUserDtoValidator

---
+++ UserManagment/Models/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Manager.Entities;
+using System.Linq;
+
+
+namespace Manager.Models.Validators
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator(ManagerDbContext dBContext)
+        {
+
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+        
+    }
+}
