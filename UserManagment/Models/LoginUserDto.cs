6047adc3d3783ff06012022f3492c13074e5488a
Paweł <prapdm@users.noreply.github.com>
2021-10-19 13:28:40 +0200

    chore: clean code

--- UserManagment/Models/LoginUserDto.cs
+++ UserManagment/Models/LoginUserDto.cs
@@ -1,9 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Manager.Models
 {
     public class LoginUserDto
     {
-        public string Email { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
