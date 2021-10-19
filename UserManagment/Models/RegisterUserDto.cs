046fb7089a10279922bf55e787af8e05debad78c
Paweł <prapdm@users.noreply.github.com>
2021-10-19 13:28:56 +0200

    chore: new prop VerifcationCode

--- UserManagment/Models/RegisterUserDto.cs
+++ UserManagment/Models/RegisterUserDto.cs
@@ -7,6 +7,7 @@ namespace Manager.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string ConfirmPassword { get; set; }
+        public int VerifcationCode { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
 
