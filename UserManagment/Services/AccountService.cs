ea5327e10ead0184d8c2c53a447e20c78d664154
Paweł <prapdm@users.noreply.github.com>
2021-10-19 13:30:04 +0200

    chore: refactor user seervice

--- UserManagment/Services/AccountService.cs
+++ UserManagment/Services/AccountService.cs
@@ -10,48 +10,85 @@ using System.Linq;
 using System.Security.Claims;
 using Manager.Entities;
 using System.Threading.Tasks;
+using FluentEmail.Core;
+using System.Reflection;
+
 
 namespace Manager.Services
 {
     public interface IAccountService
     {
-        bool LoginUser(LoginUserDto dto);
-    
+        bool VerifyPassword(LoginUserDto dto);
         void RegisterUser(RegisterUserDto dto);
+        bool VeryfiyEmail(VeryfiyEmailDto dto);
    
     }
     public class AccountService : IAccountService
     {
         private readonly ManagerDbContext _context;
-        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IPasswordHasher<UserService> _passwordHasher;
         private readonly IHttpContextAccessor _contextAccessor;
+        private readonly MailSettings _mailSettings;
+        private readonly IFluentEmail _email;
 
-        public AccountService(ManagerDbContext context, IPasswordHasher<User> passwordHasher, IHttpContextAccessor contextAccessor)
+        public AccountService(ManagerDbContext context, IPasswordHasher<UserService> passwordHasher, IHttpContextAccessor contextAccessor,
+                MailSettings mailSettings, IFluentEmail email  )
         {
             _context = context;
             _passwordHasher = passwordHasher;
             _contextAccessor = contextAccessor;
+            _mailSettings = mailSettings;
+            _email = email;
         }
+        public bool VeryfiyEmail(VeryfiyEmailDto dto)
+        {
+            var user = _context.Users
+                .Where(u => u.Email == dto.Email)
+                .Where(c => c.VerifcationCode == dto.VerifcationCode)
+                .Where(c => c.IsActive == false)
+                .FirstOrDefault();
+
+            if (user is null)
+                 return false;
+
+
+            user.IsActive = true;
+            _context.SaveChanges();
+            return true;
+        }
+
         public void RegisterUser(RegisterUserDto dto)
         {
-            var newUser = new User()
+            Random rnd = new();
+            dto.VerifcationCode = rnd.Next(10000, 99999);
+            var newUser = new UserService()
             {
                 Email = dto.Email,
                 Name = dto.Name,
                 Surname = dto.Surname,
-                RoleId = dto.RoleId
+                RoleId = dto.RoleId,
+                VerifcationCode = dto.VerifcationCode
             };
 
             var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
             newUser.PasswordHash = hashedPassword;
-            _context.User.Add(newUser);
+            _context.Users.Add(newUser);
             _context.SaveChanges();
+            var test = this.GetType().GetTypeInfo().Assembly;
+
+            var email = _email
+                .To(dto.Email)
+                .BCC(_mailSettings.AdminBCC)
+                .Subject("Confirm your addres email")
+                .UsingTemplateFromFile(@"Views\Mail\Welcome.cshtml", dto);
+ 
+            Task.Run(async () => await email.SendAsync());
 
         }
-        public  bool LoginUser(LoginUserDto dto)
+
+        public bool VerifyPassword(LoginUserDto dto)
         {
-            var user = _context.User
-                .Include(u => u.Role)
+            var user = _context.Users
                 .FirstOrDefault(u => u.Email == dto.Email);
             if (user is null)
                 return false;
@@ -60,6 +97,17 @@ namespace Manager.Services
             var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password);
             if (result == PasswordVerificationResult.Failed)
                 return false;
+
+            return Login(user.Id);
+            
+
+        }
+
+        private bool Login(int userId)
+        {
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Id == userId);
 
             var claims = new List<Claim>()
             {
@@ -75,19 +123,21 @@ namespace Manager.Services
             var authProperties = new AuthenticationProperties
             {
                 AllowRefresh = true,
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(15),
+                ExpiresUtc = DateTimeOffset.Now.AddMinutes(15),
                 IsPersistent = true,
             };
 
-            LoginAsync(new ClaimsPrincipal(claimsIdentity),authProperties);
-            return true;
+            LoginAsync(new ClaimsPrincipal(claimsIdentity), authProperties);
 
+            return true;
         }
 
         private async void LoginAsync(ClaimsPrincipal claimsPrincipal, AuthenticationProperties authentication)
         {
             await _contextAccessor.HttpContext.SignInAsync(claimsPrincipal, authentication);
         }
+
+   
 
     }
 
