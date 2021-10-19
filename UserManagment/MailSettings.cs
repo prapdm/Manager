4088bb62de3516af200c17dcf99d35c0b19b2aab
Paweł <prapdm@users.noreply.github.com>
2021-10-19 13:33:51 +0200

    chore: new mail settings

---
+++ UserManagment/MailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class MailSettings
+    {
+        public string From { get; set; }
+        public string AdminBCC { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+        public bool RequiresAuthentication { get; set; }
+
+    }
+}
