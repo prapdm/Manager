5b98273259a75b690284c7916f622216a3507a64
Paweł <prapdm@users.noreply.github.com>
2021-10-21 12:09:51 +0200

    chore: new UserService

--- UserManagment/Services/UserService.cs
+++ UserManagment/Services/UserService.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using AutoMapper;
+using Manager.Entities;
+using Manager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Manager.Services
 {
-    public class UserService
+    public interface IUserService
     {
-        
+        public List<UserDto> GetAll();
+     
+    }
+    public class UserService : IUserService
+    {
+        private readonly ManagerDbContext _dBContext;
+        private readonly IMapper _mapper;
+
+        public UserService(ManagerDbContext dBContext, IMapper mapper)
+        {
+            _dBContext = dBContext;
+            _mapper = mapper;
+        }
+        public List<UserDto> GetAll()
+        {
+            var users = _dBContext.Users
+                 .Include(u => u.Role).ToList();
+
+            var usersDtos = _mapper.Map<List<UserDto>>(users);
+
+            return usersDtos;
+        }
+
+ 
     }
 }
