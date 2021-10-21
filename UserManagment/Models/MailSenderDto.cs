dcb1c9d6b9c67e94d97e47a0d0c3298e2913ffc1
Paweł <prapdm@users.noreply.github.com>
2021-10-21 12:05:59 +0200

    chore: new dtos

---
+++ UserManagment/Models/MailSenderDto.cs
@@ -0,0 +1,18 @@
+﻿ 
+
+namespace Manager.Models
+{
+    public class MailSenderDto 
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Template { get; set; }
+        public string Subject { get; set; }
+        public string PlainBody { get; set; }
+        public int VerifcationCode { get; set; }
+        public string VerifcationToken { get; set; }
+        public string Url { get; set; }
+
+    }
+}
---
+++ UserManagment/Models/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
