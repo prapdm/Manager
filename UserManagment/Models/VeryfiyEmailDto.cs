50d81bcbd81e4ec58eddf9fae22370b8b79f2daa
Paweł <prapdm@users.noreply.github.com>
2021-10-21 12:06:43 +0200

    chore: VerifcationToken added

--- UserManagment/Models/VeryfiyEmailDto.cs
+++ UserManagment/Models/VeryfiyEmailDto.cs
@@ -5,6 +5,7 @@ namespace Manager.Models
     public class VeryfiyEmailDto
     {
         public int VerifcationCode { get; set; }
+        public int VerifcationToken { get; set; }
         public string Email { get; set; }
     }
 }
