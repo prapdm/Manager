35f035b5254ab9e9c7ae212cc85b6499f2202019
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:49:48 +0200

    chore: New Exeption

---
+++ UserManagment/Exeptions/NotFoundExeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Exeptions
+{
+    public class NotFoundExeption : Exception
+    {
+        public NotFoundExeption(string messege) : base(messege) 
+        {
+        }
+    }
+}
