065ac347ab3aa28ce27e6366b100ee0905803897
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:54:33 +0200

    chore: attributes added

--- UserManagment/Models/LoginUserDto.cs
+++ UserManagment/Models/LoginUserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Manager.Models
 {
     public class LoginUserDto
     {
+        [Required]
         public string Password { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
     }
 }
