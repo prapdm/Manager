4de26c12b4ae3c48102b2d3b58d7865ae6d97a04
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:53:52 +0200

    chore: pagination, sorting

---
+++ UserManagment/Models/Query.cs
@@ -0,0 +1,13 @@
+﻿using Manager.Entities;
+
+namespace Manager.Models
+{
+    public class Query
+    {
+        public string searchPhrase { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+        public string SortBy { get; set; } = "Id";
+        public SortDirection SortDirection { get; set; } = SortDirection.ASC;
+    }
+}
