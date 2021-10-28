3105bdcea3f2eb7fad701101f48318aef21e45ba
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:55:18 +0200

    chore: attributes added

--- UserManagment/Models/VeryfiyEmailDto.cs
+++ UserManagment/Models/VeryfiyEmailDto.cs
@@ -1,11 +1,15 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Manager.Models
 {
     public class VeryfiyEmailDto
-    {
+    {   
+        [Required]
         public int VerifcationCode { get; set; }
         public int VerifcationToken { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
     }
 }
