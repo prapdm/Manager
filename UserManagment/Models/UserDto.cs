aad3e60c60642e663de9587df92b9980a9cc021f
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:55:07 +0200

    chore: attributes added

--- UserManagment/Models/UserDto.cs
+++ UserManagment/Models/UserDto.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Manager.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +11,19 @@ namespace Manager.Models
 {
     public class UserDto
     {
+        public int Id { get; set; } 
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
-
-        public string Role { get; set; }
+        public string Phone { get; set; }
+        [Required, NotNull]
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public List<Role> Roles { get; set; }
+        [Required]
         public bool IsActive { get; set; }
     }
 }
