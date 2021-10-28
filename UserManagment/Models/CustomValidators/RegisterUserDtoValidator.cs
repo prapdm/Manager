64284996927e4511cd605344cebdda598ce77f54
Paweł <prapdm@users.noreply.github.com>
2021-10-28 14:52:55 +0200

    chore: name refactor

---
+++ UserManagment/Models/CustomValidators/RegisterUserDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System.Linq;
+using Manager.Entities;
+
+namespace Manager.Models.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(ManagerDbContext dBContext)
+        {
+
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Surname).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+            RuleFor(x => x.ConfirmPassword).NotEmpty().Equal(e => e.Password);
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dBContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "That email is taken");
+                    }
+
+                });
+        }
+    }
+}
