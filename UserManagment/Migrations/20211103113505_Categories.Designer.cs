2e9673915777360f13e76032df347e88f202ab5a
Paweł <prapdm@users.noreply.github.com>
2021-11-09 20:12:20 +0100

    chore: new migration file for Categories

--- UserManagment/Migrations/20211103113505_Categories.Designer.cs
+++ UserManagment/Migrations/20211103113505_Categories.Designer.cs
@@ -4,14 +4,16 @@ using Manager.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Manager.Migrations
 {
     [DbContext(typeof(ManagerDbContext))]
-    partial class ManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211103113505_Categories")]
+    partial class Categories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
