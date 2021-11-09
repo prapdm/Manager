038f941e54b4c493f82063cd9ffb9035453ba195
Paweł <prapdm@users.noreply.github.com>
2021-11-09 20:13:20 +0100

    chore: new category&file service

---
+++ UserManagment/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Manager.Services
+{
+    public interface IFileService
+    {
+        Task UploadAsync(IFormFile file);
+    }
+
+
+    public class FileService : IFileService
+    {
+        public FileService()
+        {
+
+        }
+
+        public async Task UploadAsync(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Path.GetFullPath("wwwroot");
+                var fileName = file.FileName;
+                var fullPath = @$"{rootPath}\images\category\{fileName}";
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+        }
+    }
+
+   
+}
