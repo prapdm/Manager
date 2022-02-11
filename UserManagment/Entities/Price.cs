2efe763784bd5febdf943cb62f665a8c088d62c1
Paweł <prapdm@users.noreply.github.com>
2022-02-11 14:56:15 +0100

    chore: new Entities

---
+++ UserManagment/Entities/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Manager.Entities
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public string SKU { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
